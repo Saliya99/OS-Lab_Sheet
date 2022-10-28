2f26e93f00539bf52a4e5c0e6997abbca59e5eb5
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2022-10-28 11:00:24 +0530

    main

--- run.c
+++ run.c
@@ -44,3 +44,30 @@ void *th2()
 
 }
 
+int main()
+{
+int n,i=0;
+pthread_t t1;
+pthread_t t2;
+pthread_t t3;
+
+	FILE *fp;
+	fp = fopen("data.txt", "r");
+	
+	while(fscanf(fp, "%d", &arr[i])!=EOF){  
+	i++; 
+       	}  
+	
+	n=pthread_create(&t1,NULL,&th,NULL);
+	pthread_join(t1,NULL);
+	//printf("\n done and my value is %d",n);
+	n=pthread_create(&t2,NULL,&th1,NULL);
+        pthread_join(t2,NULL);
+	//printf("\n done and my value is %d",n);
+	n=pthread_create(&t3,NULL,&th2,NULL);
+        pthread_join(t3,NULL);
+	//printf("\n done and my value is %d",n);
+
+	fclose(fp);
+
+}
